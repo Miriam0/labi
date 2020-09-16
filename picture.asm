03251b6eb83c848cf02e732a63ba3affc3a83017
Miriam0 <46651542+Miriam0@users.noreply.github.com>
2020-09-16 10:56:02 +0300

    Create picture.asm

---
+++ picture.asm
@@ -0,0 +1,22 @@
+system 1
+mov 0180,r0
+out r0,p0
+mov 0240,r1
+out r1,p1
+mov 04E0,r2
+out r2,p2
+mov 0810,r3
+out r3,p3
+mov 1FF8,r0
+out r0,p4
+mov 1008,r1
+out r1,p5
+mov 1008,r2
+out r2,p6
+mov 1FF8,r3
+out r3,p7
+
+show:
+system 6
+jnz show
+stop
